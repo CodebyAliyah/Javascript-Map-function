821f9609f039da6009c4447e5c7e1c5d698402dc
Ali Yahya <165564206+CodebyAliyah@users.noreply.github.com>
2024-09-02 23:29:04 -0700

    Code Successful

---
+++ map.js
@@ -0,0 +1,29 @@
+
+const myNumbers = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
+
+// let newNums = myNumbers.map( (item) => {
+//   return item * 10 ()  .map ( (item) => {
+//     return item + 1 () .filter( (item) => {
+//        return item >=70 ;
+//     } )
+
+//   } )
+// } )
+
+// console.log(newNums)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
